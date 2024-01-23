1fde0d942a107b844eaa0921fa093c060f5e63ee
Aditya <kohsa9211420@gmail.com>
2024-01-23 19:31:19 +0530

    first commit

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='border-2 border-gray-500 flex flex-col mt-28 justify-center items-center text-center'>
+        <img src="checked.png" alt="" className='w-1/2 mx-auto'/> 
+        <h1  className='items-center text-center m-auto mt-20 text-3xl font-bold'>Successfully logged in</h1>
+    </div>
+  )
+}
+
+export default Home
